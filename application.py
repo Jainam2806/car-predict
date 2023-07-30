b04bf2e6d064361fa115b817a73f7b1575f8e516
Jainam Parekh <121747559+Jainam2806@users.noreply.github.com>
2023-07-31 01:27:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -30,4 +30,4 @@ def predict():
     prediction = model.predict(pd.DataFrame([[car_model, company, year, kms_driven, fuel_type]], columns=['name', 'company', 'year', 'kms_driven', 'fuel_type']))
     return str(np.round(prediction[0],-2))
 if __name__ == "__main__":
-    app.run(port=port) 
+    app.run(host="0.0.0.0",port=port) 
